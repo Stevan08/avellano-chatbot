fdf1a636f3c14311947ee8ed58c85eea5ce24f5b
Stevan08 <stevanfernandez2004@gmail.com>
2025-10-29 19:00:49 -0500

    se modificaron los archivos necesarios para iniciar la aplicación desde src/app.js en lugar de src/server.js, y se eliminaron las variables de entorno del archivo de configuración render.yaml.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-// src/app.js
 import express from "express";
 import bodyParser from "body-parser";
 import webhookRoutes from "./routes/webhookRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
 
 const app = express();
-
 app.use(bodyParser.json());
-app.use("/", webhookRoutes);
+app.use("/webhook", webhookRoutes);
+app.use("/client", clientRoutes);
 
-export default app;
+app.get("/", (req, res) => {
+  res.send("Servidor Avellano corriendo 🚀");
+});
+
+// Render define process.env.PORT automáticamente
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
--- src/server.js
+++
@@ -1,8 +0,0 @@
-// src/server.js
-import dotenv from "dotenv";
-import app from "./app.js";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
